5e804ab602b0d7ba84a9784a747dc2355c98e301
Anthony Gbobo <psalmseins@gmail.com>
2023-10-20 21:02:18 -0700

    Correction on 13-print_revs.c

--- 13-print_revs.c
+++ 13-print_revs.c
@@ -2,18 +2,22 @@
 /**
  * print_revs - print string in revers
  * @val: arguments
- * Return: return string
+ * Return: return i
  */
 
 int print_revs(va_list val)
 {
+	int i = 0, j;
 	char *s = va_arg(val, char*);
-	int i;
-	int j = 0;
 
-	while (s[j] != '\0')
-		j++;
-	for (i = j - 1; i >= 0; i--)
-		_putchar(s[i]);
-	return (j);
+	if (!s)
+		s = "(null)";
+
+	while (s[i])
+		i++;
+
+	for (j = i - 1; j >= 0; j--)
+		_putchar(s[j]);
+
+	return (i);
 }
